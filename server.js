0aa59342c15c8a4bafe9730a2c0bf2186f574a49
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2020-07-26 00:36:42 +1300

    script change

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid')
-const { Socket } = require('dgram')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -28,3 +27,4 @@ io.on('connection', (socket) => {
 })
 
 server.listen('https://zoom-video-call.herokuapp.com/')
+//https://zoom-video-call.herokuapp.com/
