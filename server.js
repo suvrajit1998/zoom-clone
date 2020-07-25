40c7d533569164630010f4dc39582f0094eb4f6e
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2020-07-26 08:57:53 +1300

    add cors routing

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid')
+const cors = require('cors')
 
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
